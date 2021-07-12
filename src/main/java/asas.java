bab5323200d6fd859a72d0d16b110df5a74bb947
ZhangJiangNan <3438620135@qq.com>
2021-07-12 14:47:29 +0800

    master修改

--- src/main/java/asas.java
+++ src/main/java/asas.java
@@ -5,6 +5,7 @@ public class asas {
         System.out.println("bbb");
         System.out.println("ccc");
         System.out.println("ddd");
-        System.out.println("hot-fix修改");
+        System.out.println("hot-fix修改01");
+        System.out.println("master修改");
     }
 }
